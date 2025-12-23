ee9f0a860e1de3f82bcde74b52daf9ce0c2984d6
panthercomputers <thepanthercomputers@gmail.com>
2025-12-23 10:27:39 -0600

    Update onvif_config.js
    Prevents overlapping ONVIF health checks
    
    Prevents dangling admin HTTP requests
    
    Prevents race-condition initialization
    
    Prevents listener leaks
    
    Ensures password masking works correctly
    
    Ensures Node-RED reconnects cleanly instead of crashing

--- onvif_config.js
+++ onvif_config.js
@@ -18,214 +18,220 @@
  * limitations under the License.
  */
 
- module.exports = function(RED) {
-    var settings = RED.settings;
-    const onvif  = require('onvif');
-    
-    function setOnvifStatus(node, onvifStatus) {
-        node.onvifStatus = onvifStatus;
-        // Pass the new status to all the available listeners
-        node.emit('onvif_status', onvifStatus);
+module.exports = function (RED) {
+    const onvif = require("onvif");
+
+    const STATUS = {
+        CONNECTED: "connected",
+        DISCONNECTED: "disconnected",
+        INITIALIZING: "initializing",
+        UNCONFIGURED: "unconfigured"
+    };
+
+    function setOnvifStatus(node, status) {
+        node.onvifStatus = status;
+        node.emit("onvif_status", status);
     }
 
     function OnVifConfigNode(config) {
         RED.nodes.createNode(this, config);
-        this.xaddress                = config.xaddress;
-        this.port                    = parseInt(config.port || 80);
-        this.name                    = config.name;
-        this.timeout                 = config.timeout || 3;
-        this.checkConnectionInterval = config.checkConnectionInterval || 5;
-        // Remark: user name and password are stored in this.credentials
-        
-        var node = this;
-        
-        // All Onvif nodes can add a listener to track the 'onvif_status' events.
-        // However by default only 10 listeners are allowed, which results in a warning when more Onvif nodes use this config node:
-        // MaxListenersExceededWarning: Possible EventEmitter memory leak detected
-        // To avoid that, we will allow an infinite number of listeners.
-        // Caution: when you have suspicion that the listeners are leaking, put the next line in comment !!!
-        node.setMaxListeners(0);
-        
-        this.getProfiles = function(clientConfig, response) {
-            var profileNames = [];
-            var config = {};
-            
-            // TODO checken of er altijd een this.credentials bestaat, indien username en paswoord niet ingevuld is.
-            
-            // The client credentials will only contain the data (i.e. user name or password) which has changed.
-            // The other data is not changed, so we will need use the original data stored on the server.
-            clientConfig.username = clientConfig.user || this.credentials.user;
-            clientConfig.password = clientConfig.password || this.credentials.password;
-                    
-            // When the user appends some new text to the existing password, then the original password is passed via the client as __PWRD__
-            // So replace __PWRD__ again by the original password.
-            if (clientConfig.password && this.credentials.password) {
-               clientConfig.password.replace('___PWRD__', this.credentials.password);
+
+        this.xaddress = config.xaddress;
+        this.port = parseInt(config.port || 80, 10);
+        this.name = config.name;
+        this.timeout = Number(config.timeout || 3);
+        this.checkConnectionInterval = Number(config.checkConnectionInterval || 5);
+
+        const node = this;
+
+        // Prevent listener warnings without disabling leak detection entirely
+        node.setMaxListeners(50);
+
+        node.cam = null;
+        node._initializing = false;
+        node._checking = false;
+        node.checkConnectionTimer = null;
+
+        /* ------------------------------------------------------------
+         * Profiles helper (used by editor UI)
+         * ---------------------------------------------------------- */
+        this.getProfiles = function (clientConfig, response) {
+            const profileNames = [];
+
+            const creds = this.credentials || {};
+
+            clientConfig.username = clientConfig.user || creds.user;
+            clientConfig.password = clientConfig.password || creds.password;
+            clientConfig.hostname = clientConfig.hostname || this.xaddress;
+            clientConfig.port = clientConfig.port || this.port;
+            clientConfig.timeout = (this.timeout || 3) * 1000;
+
+            // Restore masked password if needed
+            if (clientConfig.password && creds.password) {
+                clientConfig.password =
+                    clientConfig.password.replace("___PWRD__", creds.password);
             }
-     
-            if (this.credentials.user !== clientConfig.user || this.credentials.password !== clientConfig.password || this.xaddress !== clientConfig.hostname){
-                var cam = new onvif.Cam(clientConfig, function(err) {             
-                    if (!err) {
-                        if (cam.profiles) {
-                            for(var i = 0; i < cam.profiles.length; i++) {
-                                profileNames.push({
-                                    label: cam.profiles[i].name,
-                                    value: cam.profiles[i].$.token
-                                });
-                            }
-                        }
-                        
-                        response.json(profileNames);
-                    }
-                });
-            }
-            else {
-                if (this.cam.profiles) {
-                    // The current deployed cam is still up-to-date, so let’s use that one (for performance reasons)
-                    for(var i = 0; i < this.cam.profiles.length; i++) {
+
+            const sameAuth =
+                creds.user === clientConfig.username &&
+                creds.password === clientConfig.password &&
+                this.xaddress === clientConfig.hostname &&
+                this.port === clientConfig.port;
+
+            const finish = (cam) => {
+                if (cam && cam.profiles) {
+                    for (let i = 0; i < cam.profiles.length; i++) {
                         profileNames.push({
-                            label: this.cam.profiles[i].name,
-                            value: this.cam.profiles[i].$.token
+                            label: cam.profiles[i].name,
+                            value: cam.profiles[i].$.token
                         });
                     }
                 }
-                
                 response.json(profileNames);
-            }
-        }
-        
-        this.getProfileTokenByName = function(profileName) {
-            if (this.cam.profiles) {
-                // Try to find a profile with the specified name, and return the token
-                for(var i = 0; i < this.cam.profiles.length; i++) {
-                    if (this.cam.profiles[i].name === profileName) {
-                        return this.cam.profiles[i].$.token;
+            };
+
+            if (!sameAuth) {
+                new onvif.Cam(clientConfig, function (err) {
+                    if (err) {
+                        response.status(500).json([]);
+                        return;
                     }
+                    finish(this);
+                });
+            } else {
+                finish(this.cam);
+            }
+        };
+
+        this.getProfileTokenByName = function (profileName) {
+            if (!this.cam || !this.cam.profiles) return null;
+
+            for (let i = 0; i < this.cam.profiles.length; i++) {
+                if (this.cam.profiles[i].name === profileName) {
+                    return this.cam.profiles[i].$.token;
                 }
             }
-            
-            // No token found with the specified name
             return null;
-        }
-       
-        // This should be called by all nodes that use this config node
-        this.initialize = function() {
-            // This config node can only be initialized oncellchange
-            if (this.cam) {
-                return;
-            }
-           
-            // Without an xaddress, it is impossible to connect to an Onvif device
+        };
+
+        /* ------------------------------------------------------------
+         * Initialization / connection
+         * ---------------------------------------------------------- */
+        this.initialize = function () {
+            if (this.cam || this._initializing) return;
+
             if (!this.xaddress) {
-                // Make sure the Catch-node can catch the error
-                node.error( "Cannot connect to unconfigured Onvif device", {} );
-                    
-                this.cam = null;
-                setOnvifStatus(node, "unconfigured");
+                node.error("Cannot connect to unconfigured ONVIF device", {});
+                setOnvifStatus(node, STATUS.UNCONFIGURED);
                 return;
             }
-            
-            setOnvifStatus(node, "initializing");
-            
-            var options = {};
-            options.hostname = this.xaddress;
-            options.port = this.port;
-            options.timeout = this.timeout * 1000;
-            
+
+            this._initializing = true;
+            setOnvifStatus(node, STATUS.INITIALIZING);
+
+            const options = {
+                hostname: this.xaddress,
+                port: this.port,
+                timeout: this.timeout * 1000
+            };
+
             if (this.credentials && this.credentials.user) {
                 options.username = this.credentials.user;
                 options.password = this.credentials.password;
             }
 
-            // Create a new camera instance, which will automatically connect to the device (to load configuration data)
-            this.cam = new onvif.Cam(options, function(err) { 
+            this.cam = new onvif.Cam(options, (err) => {
+                this._initializing = false;
+
                 if (err) {
-                    // Make sure the Catch-node can catch the error
-                    node.error( err, {} );
-                    
-                    setOnvifStatus(node, "disconnected");
-                }
-                else {  
-                    setOnvifStatus(node, "connected"); 
+                    node.error(err, {});
+                    setOnvifStatus(node, STATUS.DISCONNECTED);
+                } else {
+                    setOnvifStatus(node, STATUS.CONNECTED);
                 }
             });
-            
-            // When an checkConnection timer is running, then stop it
+
             if (this.checkConnectionTimer) {
                 clearInterval(this.checkConnectionTimer);
                 this.checkConnectionTimer = null;
             }
-            
+
             if (this.checkConnectionInterval > 0) {
-                // Start a new checkConnection timer, that checks at the specified interval whether the Onvif device is disconnected.
-                // This way we can keep the node status in the flow editor up to date ...
-                this.checkConnectionTimer = setInterval(function() {
-                    // Check whether the Onvif device is connected, by calling the device system and time
-                    node.cam.getSystemDateAndTime(function(err, date, xml) {
+                this.checkConnectionTimer = setInterval(() => {
+                    if (!node.cam || node._checking) return;
+
+                    node._checking = true;
+
+                    node.cam.getSystemDateAndTime((err) => {
+                        node._checking = false;
+
                         if (err) {
-                            setOnvifStatus(node, "disconnected");
+                            setOnvifStatus(node, STATUS.DISCONNECTED);
+                            return;
                         }
-                        else {
-                            if (!node.cam.capabilities && !node.cam.services) {
-                                // Probably the device was unavailable when this node was being started, so the autoConnect was not able
-                                // to load all the data from the camera (like e.g. its capabilities).  Afterwards the device became available,
-                                // so it is now time to load the device data into the Cam instance (by calling its 'connect' function).
-                                // Note that this needs to be executed before setOnvifStatus, because that function uses the capabilities.
-                                node.cam.connect(function(err, date, xml) {
-                                    if (err) {
-                                        node.error("The device is now connected, but the data cannot be loaded");
-                                    }
-                                });
-                            }
-                            
-                            setOnvifStatus(node, "connected");
+
+                        if (!node.cam.capabilities && !node.cam.services) {
+                            node.cam.connect((connectErr) => {
+                                if (connectErr) {
+                                    node.error(
+                                        "Device connected but capabilities could not be loaded"
+                                    );
+                                }
+                            });
                         }
+
+                        setOnvifStatus(node, STATUS.CONNECTED);
                     });
                 }, this.checkConnectionInterval * 1000);
             }
-        }
-        
-        node.on('close', function(){
-			setOnvifStatus(this, "");
-            
+        };
+
+        /* ------------------------------------------------------------
+         * Cleanup
+         * ---------------------------------------------------------- */
+        node.on("close", function () {
+            setOnvifStatus(this, "");
+
             this.removeAllListeners("onvif_status");
-            
-            // When an checkConnection timer is running, then stop it
+
             if (this.checkConnectionTimer) {
                 clearInterval(this.checkConnectionTimer);
                 this.checkConnectionTimer = null;
             }
-		});
+
+            this.cam = null;
+            this._checking = false;
+            this._initializing = false;
+        });
     }
-    
-    RED.nodes.registerType("onvif-config",OnVifConfigNode,{
+
+    RED.nodes.registerType("onvif-config", OnVifConfigNode, {
         credentials: {
-            user: {type:"text"},
-            password: {type: "password"}
+            user: { type: "text" },
+            password: { type: "password" }
         }
     });
-        
-    // Make all the available profiles accessible for the node's config screen
-    RED.httpAdmin.get('/onvifdevice/:cmd/:config_node_id', RED.auth.needsPermission('onvifdevice.read'), function(req, res){
-        var configNode = RED.nodes.getNode(req.params.config_node_id);
 
-        switch (req.params.cmd) {
-            case "profiles":
-                if (!configNode) {
-                    console.log("Cannot determine profile list from node " + req.params.config_node_id);
-                    return;
-                }
-               
-                // Get the profiles of the camera, based on the config data on the client, instead of the config data
-                // stored inside this config node.  Reason is that the config data on the client might be 'dirty', i.e. changed
-                // by the user but not deployed yet on this config node.  But the client still needs to be able to get the profiles
-                // corresponding to that dirty config node.  That way the config screen can be filled with profiles already...
-                // But when the config data is not dirty, we will just use the profiles already loaded in this config node (which is faster).
-                // See https://discourse.nodered.org/t/initializing-config-screen-based-on-new-config-node/7327/10?u=bartbutenaers
-                configNode.getProfiles(req.query, res);
-                
-                break;
+    /* ------------------------------------------------------------
+     * Admin HTTP endpoint (editor UI)
+     * ---------------------------------------------------------- */
+    RED.httpAdmin.get(
+        "/onvifdevice/:cmd/:config_node_id",
+        RED.auth.needsPermission("onvifdevice.read"),
+        function (req, res) {
+            const configNode = RED.nodes.getNode(req.params.config_node_id);
+            if (!configNode) {
+                res.status(404).json([]);
+                return;
+            }
+
+            switch (req.params.cmd) {
+                case "profiles":
+                    configNode.getProfiles(req.query, res);
+                    break;
+                default:
+                    res.status(400).json([]);
+            }
         }
-    });
-}
-
+    );
+};
