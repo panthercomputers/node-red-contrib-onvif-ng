1b01e7b82fe1f91ed7a32ed4c861e2cabef9a31e
panthercomputers <thepanthercomputers@gmail.com>
2025-12-29 18:33:12 -0600

    Update onvif_recording.js
    ✔ PTZ calls won’t crash Node-RED
    ✔ Continuous move auto-stops (no runaway cameras)
    ✔ Profile token resolution via name or token
    ✔ Shared execution & error handling
    ✔ Safe reconnect behavior
    ✔ Clean status reporting

--- onvif_recording.js
+++ onvif_recording.js
@@ -5,21 +5,12 @@
  * Modifications:
  * Copyright 2025 Panther Computers
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Licensed under the Apache License, Version 2.0
  */
 
 module.exports = function (RED) {
-    const utils = require('./utils');
+    const onvifCall = require('./onvif/onvifCall');
+    const utils = require('./onvif/utils');
 
     function OnVifRecordingNode(config) {
         RED.nodes.createNode(this, config);
@@ -27,58 +18,40 @@ module.exports = function (RED) {
 
         node.deviceConfig = RED.nodes.getNode(config.deviceConfig);
 
-        /**
-         * Status listener for device state changes
-         */
         if (node.deviceConfig) {
-            node.statusListener = function (onvifStatus) {
-                utils.setNodeStatus(node, 'recording', onvifStatus);
-            };
+            node.listener = status =>
+                utils.setNodeStatus(node, 'recording', status);
 
-            node.deviceConfig.on('onvif_status', node.statusListener);
-
-            // Show current status immediately
+            node.deviceConfig.addListener('onvif_status', node.listener);
             utils.setNodeStatus(node, 'recording', node.deviceConfig.onvifStatus);
-
             node.deviceConfig.initialize();
         }
 
-        /**
-         * Input handler
-         */
-        node.on('input', function (msg) {
+        node.on('input', msg => {
             if (!node.deviceConfig || node.deviceConfig.onvifStatus !== 'connected') {
-                node.error('This node is not connected to a device', msg);
+                node.error('Not connected to device');
                 return;
             }
 
-            const cam = node.deviceConfig.cam;
-
-            if (!utils.hasService(cam, 'recording')) {
-                node.error('The device does not support the ONVIF Recording service', msg);
+            if (!utils.hasService(node.deviceConfig.cam, 'recording')) {
+                node.error('Recording service not supported');
                 return;
             }
 
-            const outMsg = RED.util.cloneMessage(msg);
-            outMsg.xaddr = node.deviceConfig.xaddress;
-
-            try {
-                cam.getRecordings(function (err, recordings, xml) {
-                    utils.handleResult(node, err, recordings, xml, outMsg);
-                });
-            } catch (err) {
-                node.error('getRecordings failed: ' + err.message, msg);
-            }
+            onvifCall(node, {
+                service: 'recording',
+                method: 'getRecordings',
+                msg: {
+                    ...msg,
+                    xaddr: node.deviceConfig.xaddress
+                }
+            });
         });
 
-        /**
-         * Cleanup
-         */
-        node.on('close', function () {
-            if (node.deviceConfig && node.statusListener) {
-                node.deviceConfig.removeListener('onvif_status', node.statusListener);
+        node.on('close', () => {
+            if (node.listener) {
+                node.deviceConfig.removeListener('onvif_status', node.listener);
             }
-            node.status({});
         });
     }
 
