23f8e30edf4d31000b24dd151cf291f1834b77b7
panthercomputers <thepanthercomputers@gmail.com>
2025-12-31 12:16:19 -0600

    Update onvif_device.js

--- nodes/onvif/onvif_device.js
+++ nodes/onvif/onvif_device.js
@@ -9,8 +9,8 @@
  */
 
 module.exports = function (RED) {
-    const onvifCall = require('onvifCall');
-    const utils = require('utils');
+    const onvifCall = require('./onvifCall');
+    const utils = require('./utils');
 
     function OnVifDeviceNode(config) {
         RED.nodes.createNode(this, config);
@@ -80,5 +80,6 @@ module.exports = function (RED) {
 
     RED.nodes.registerType('onvif-device', OnVifDeviceNode);
 };
+
 
 
