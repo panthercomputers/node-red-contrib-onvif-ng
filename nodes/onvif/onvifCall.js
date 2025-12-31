08cabcd064d1718554e58d9c5f9ef89564098302
panthercomputers <thepanthercomputers@gmail.com>
2025-12-30 18:31:42 -0600

    Update onvifCall.js

--- nodes/onvif/onvifCall.js
+++ nodes/onvif/onvifCall.js
@@ -1,4 +1,17 @@
-'use strict';
+<!--
+  Original work:
+  Copyright 2018 Bart Butenaers
+
+  Modifications:
+  Copyright 2025 Panther Computers
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+-->
+    'use strict';
 
 module.exports = async function onvifCall(node, options) {
     const {
